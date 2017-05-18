e570706ad93402d27202f24f8619fc4492089e51
Raoni Caselli <raonicaselli@gmail.com>
2017-05-18 13:19:15 -0300

    Add portfolio-project page

--- js/src/main.js
+++ js/src/main.js
@@ -6,13 +6,36 @@ jQuery(document).ready(function($){
   var logoScroll = $(window).height() / 1.2;
 
   $(window).scroll(function(){
-    var topScroll = $(document).scrollTop();
+    var topScroll = $(this).scrollTop();
 
     if (topScroll > logoScroll){
       $(".header").addClass("visible");
     } else {
       $(".header").removeClass("visible");
     }
+
+    var about = $("#about").offset().top - 64;
+    var portfolio = $("#portfolio").offset().top - 64;
+    var design = $("#process").offset().top - 64;
+    var contact = $("#contact").offset().top - 64;
+
+    if (topScroll >= about && topScroll < portfolio){
+      $("a").removeClass("active");
+      $("a[href='#about']").addClass("active");
+    } else if (topScroll >= portfolio && topScroll < design) {
+      $("a").removeClass("active");
+      $("a[href='#portfolio']").addClass("active");
+    } else if (topScroll >= design && topScroll < contact) {
+      $("a").removeClass("active");
+      $("a[href='#process']").addClass("active");
+    } else if (topScroll >= contact) {
+      $("a").removeClass("active");
+      $("a[href='#contact']").addClass("active");
+    } else {
+      $("a").removeClass("active");
+      $("a[href='#home']").addClass("active");
+    }
+
   });
 
   // Add slider functions
@@ -31,6 +54,24 @@ jQuery(document).ready(function($){
       next: "<a class='slider-arrow next mdi mdi-chevron-right' alt='Next slide'></a>",
     }
   });
+
+  // Add class to label in form
+  $(".input").focus(function(){
+    $(this).prev(["label"]).addClass("active");
+  }).focusout(function(){
+    $(this).prev(["label"]).removeClass("active");
+  });
+
+
+  // Open portfolio projects
+  $("#open-dolphin").click(function(e){
+    e.preventDefault();
+    $("#project-page").load("html/projects/dolphin.html", function(){
+      $(".section-wrapper").addClass("visible");
+    });
+    $("html").addClass("noscroll");
+  });
+
 
 
 });
